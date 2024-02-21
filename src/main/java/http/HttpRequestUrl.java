8c008bb475a12ea669c3a244041b3464e5e1ded1
kimminkyeu <minkyeki@student.42seoul.kr>
2024-02-21 18:37:52 +0900

    feat: HttpUrl type 기본 구현

---
+++ src/main/java/http/HttpRequestUrl.java
@@ -0,0 +1,29 @@
+package http;
+
+public class HttpRequestUrl {
+  private String innerUrl;
+
+  public HttpRequestUrl(final String url) {
+    this.innerUrl = url;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    HttpRequestUrl that = (HttpRequestUrl) o;
+
+    return innerUrl.equals(that.innerUrl);
+  }
+
+  @Override
+  public String toString() {
+    return innerUrl;
+  }
+
+  @Override
+  public int hashCode() {
+    return innerUrl.hashCode();
+  }
+}
