982dd6de346b5df0065d5445beca1fe03b79bbee
kimminkyeu <minkyeki@student.42seoul.kr>
2024-02-21 19:03:19 +0900

    fix : Thread 생성후 실행하는 클래스임으로 이름에 Thread를 붙히는게 더 명확하다.

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -20,14 +20,17 @@ public class WebServer {
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
 
+        /* NOTE: try catch에서 server socket 자원을 안전하기 반환하기 위한 문법 { try (...) }
+           Ref: https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html */
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             log.info("Web Application Server started {} port.", port);
 
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
+            // NOTE: accept가 null을 반환하는 경우가 뭐지?
             while ((connection = listenSocket.accept()) != null) {
-                RequestHandler requestHandler = new RequestHandler(connection);
-                requestHandler.start();
+                RequestHandlerThread requestHandlerThread = new RequestHandlerThread(connection);
+                requestHandlerThread.start(); // --> start thread (요청마다 쓰레드를 생성한다...?)
             }
         }
     }
